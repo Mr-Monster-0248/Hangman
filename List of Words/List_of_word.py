482110f2b2b92d9a3a411cd7006ab3a77ab271cc
Mr-Monster-0248 <tibolepezfr@gmail.com>
2017-10-27 16:21:33 +0200

    first commit

---
+++ List of Words/List_of_word.py
@@ -0,0 +1,14 @@
+from fonctions import *
+
+listOfWords = ["Hello", "Water", "Bread", "Phone", "Computer", "Water"]
+
+#dispWord(listOfWords)
+#dispItems(listOfWords)
+#removeAll(listOfWords, "Water")
+
+replace(listOfWords, "Hello", "Bite")
+
+print(listOfWords.index("Water"))
+
+print(listOfWords)
+print("\n\n")
---
+++ List of Words/fonctions.py
@@ -0,0 +1,61 @@
+#Display the words of the list where the first and the last characters have been removed
+def dispWord(listOfWords):
+
+    """Display the words of the list where the first and the last characters have been removed"""
+
+    for word in listOfWords:
+        print(word[1:-1])
+
+#Find the longest word of the list
+def longest(listOfWords):
+
+    """Find the longest word of the list"""
+
+    larger = ""
+    maxLen = 0
+    for word in listOfWords:
+        if(len(word) > maxlen):
+            maxLen = len(word)
+            larger = word
+
+    return larger
+
+#Creat a list of integers representing the lenght of the corresponding word in the list
+def listLenght(listOfWords):
+
+    """Creat a list of integers representing the lenght of the corresponding word in the list"""
+
+    lenghtOfWords = []
+    for word in listOfWords:
+        lenghtOfWords.append(len(word))
+
+    return lenghtOfWords
+
+#Display the number of item in the list followed by elements of the list
+def dispItems(listOfWords):
+
+    """Display the number of item in the list followed by elements of the list"""
+
+    print("There is {} item in the list, they are:".format(len(listOfWords)))
+    for word in listOfWords:
+        print("{word}, ".format(word = word), end="")
+
+#Removes all the occurrence of a given word
+def removeAll(listOfWords, toRemove):
+
+    """Removes all the occurrence of a given word"""
+
+    if((toRemove in listOfWords) == False):
+        print("{toRemove} isn't in the list".format(toRemove = toRemove))
+    while toRemove in listOfWords:
+        listOfWords.remove(toRemove)
+
+#Replace a word $w_1$ by a word $w_2$
+def replace(listOfWords, w_1, w_2):
+
+    """Replace a word $w_1$ by a word $w_2$"""
+
+    if(not(w_1 in listOfWords)):
+        print("{word} isn't in the list".format(word = w_1))
+    else:
+        listOfWords[listOfWords.index(w_1)] = w_2
