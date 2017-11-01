217b4575d42ce4615388b610ec1933dd356ffb77
Mr-Monster-0248 <tibolepezfr@gmail.com>
2017-11-01 12:07:56 +0100

    Added drawings

--- Hangman/draw.py
+++ Hangman/draw.py
@@ -1,1 +1,70 @@
-def 
+def fig6:
+    print("   ------")
+    print("   |    |")
+    print("  ( )   |")
+    print(" / | \  |")
+    print("/  |  \ |")
+    print("  / \   |")
+    print(" /   \  |")
+    print("        |")
+    print(" _______|___")
+
+#First sketch no error
+def fig1:
+    print("   ------")
+    print("   |    |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print(" _______|___")
+
+#Sketch 2 1 error
+def fig2:
+    print("   ------")
+    print("   |    |")
+    print("  ( )   |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print(" _______|___")
+
+#Sketch 3 2 errors
+def fig3:
+    print("   ------")
+    print("   |    |")
+    print("  ( )   |")
+    print("   |    |")
+    print("   |    |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print(" _______|___")
+
+#Sketch 4 3 errors
+def fig4:
+    print("   ------")
+    print("   |    |")
+    print("  ( )   |")
+    print(" / |    |")
+    print("/  |    |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print(" _______|___")
+
+#Sketch 5 4 errors
+def fig5:
+    print("   ------")
+    print("   |    |")
+    print("  ( )   |")
+    print(" / | \  |")
+    print("/  |  \ |")
+    print("        |")
+    print("        |")
+    print("        |")
+    print(" _______|___")
