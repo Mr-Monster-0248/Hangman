21254bb5c02429a950ecf54b7564e64274f9b0a8
Mr-Monster-0248 <tibolepezfr@gmail.com>
2017-11-04 12:37:13 +0100

    Implemented multyplayer mode

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,9 +3,7 @@ import game
 import draw
 from os import system
 
-choice = -1
 errors = 0
-win = False
 
 #1 for ia
 #2 for 2 player
@@ -25,34 +23,35 @@ while(game_mode != 1 and game_mode != 2):
     print("2 for 2 player")
     game_mode = security.input_choice()
 
-system("clear")
 if(game_mode == 1): #One player mode
     secret_w = game.choose_word_ia()
-    while(game.word_diff(secret_w, player_w) and errors < 6):
-        print("Letter you have gessed: ")
-        print(" ".join(gessed))
-        print("You have the right to make up to {} errors\n".format(6 - errors))
-
-        draw.disp_hang(errors)
-        player_w = game.initialize_player_w(secret_w, gessed)
-        print(player_w)
-
-        gess = " "
-        while(gess in gessed):
-            gess = security.input_char("Your gess: ")
-        gessed.append(gess)
-        if(not(gess in secret_w)):
-            errors += 1
-        player_w = game.initialize_player_w(secret_w, gessed)
-        system("clear")
-
-    if(not game.word_diff(secret_w, player_w)):
-        print(player_w)
-        print("You win !!")
-    else:
-        draw.disp_hang(errors)
-        print("Sorry... whant to try again ?")
-
-
 elif(game_mode == 2):
-    print("work in progress")
+    secret_w = game.choose_word_player()
+
+system("clear")
+
+while(game.word_diff(secret_w, player_w) and errors < 6):
+    print("Letter you have gessed: ")
+    print(" ".join(gessed))
+    print("You have the right to make up to {} errors\n".format(6 - errors))
+
+    draw.disp_hang(errors)
+    player_w = game.initialize_player_w(secret_w, gessed)
+    print(player_w)
+
+    gess = " "
+    while(gess in gessed):
+        gess = security.input_char("Your gess: ")
+    gessed.append(gess)
+    if(not(gess in secret_w)):
+        errors += 1
+    player_w = game.initialize_player_w(secret_w, gessed)
+    system("clear")
+
+if(not game.word_diff(secret_w, player_w)):
+    print(player_w)
+    print("You win !!")
+else:
+    print("The word was {}".format(secret_w))
+    draw.disp_hang(errors)
+    print("Sorry... whant to try again ?")
