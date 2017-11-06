23c155060781aeaacdc1b9e1d9a848ceeaf3d2e1
Mr-Monster-0248 <tibolepezfr@gmail.com>
2017-11-06 08:52:56 +0100

    Improved the security inputs

--- Hangman/security.py
+++ Hangman/security.py
@@ -1,14 +1,16 @@
 #Function to input safely only one char
 #It also check if input is a char and not a number
 def input_char(text):
-    char = str(input(text))
-    char = char[0]
-    return char
+    char = "1"
+    while(not char.isalpha()):
+        char = str(input(text))
+        char = char[0]
+    return char.upper()
 
 def input_word(text):
     print("Only your first word will be consider")
     word = str(input(text)).strip().split(" ")
-    return word[0]
+    return word[0].upper()
 
 def input_choice():
     try:
